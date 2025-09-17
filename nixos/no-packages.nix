b12af95706f2e7db1c8638ac3733d04d5f6889d8
theeasternfurry <theeasternfurry@outlook.com>
2025-09-17 07:30:03 +0700

    update(dell-vostro-intel): remove nixos manual and man page

--- nixos/no-packages.nix
+++ nixos/no-packages.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  documentation = {
+    man.enable = true;
+    nixos.enable = false;
+  };
   programs.nano.enable = false;
   services.xserver.excludePackages = with pkgs; [
     gnome-console
