22eb53bdcd2a067a82230cdb84f0b0515ba22ab2
theeasternfurry <theeasternfurry@outlook.com>
2026-01-15 13:38:22 +0700

    nixos(amd-pc): update ollama config

--- nixos/services/ollama.nix
+++ nixos/services/ollama.nix
@@ -1,9 +1,12 @@
+{ pkgs, ... }:
+
 {
   services.ollama = {
     enable = true;
     openFirewall = true;
     host = "0.0.0.0";
     port = 11434;
+    package = pkgs.ollama-rocm;
     rocmOverrideGfx = "10.3.0";
     environmentVariables = {
       OLLAMA_NUM_PARALLEL = "1"; # Chạy 1 người dùng thôi để tiết kiệm VRAM
