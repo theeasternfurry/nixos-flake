7ceb77c06d83db20b0fa920a52154b4af04bf3d3
theeasternfurry <theeasternfurry@outlook.com>
2026-01-29 09:05:05 +0700

    nixos: nginx allow cloudflare for protection and fix error

--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -3,6 +3,29 @@
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+    appendHttpConfig = ''
+      limit_req_zone $binary_remote_addr zone=one:10m rate=5r/s;
+    '';
+    commonHttpConfig = ''
+      # Cloudflare IPv4 CIDRs
+      set_real_ip_from 103.21.244.0/22;
+      set_real_ip_from 103.22.200.0/22;
+      set_real_ip_from 103.31.4.0/22;
+      set_real_ip_from 104.16.0.0/13;
+      set_real_ip_from 104.24.0.0/14;
+      set_real_ip_from 108.162.192.0/18;
+      set_real_ip_from 131.0.72.0/22;
+      set_real_ip_from 141.101.64.0/18;
+      set_real_ip_from 162.158.0.0/15;
+      set_real_ip_from 172.64.0.0/13;
+      set_real_ip_from 173.245.48.0/20;
+      set_real_ip_from 188.114.96.0/20;
+      set_real_ip_from 190.93.240.0/20;
+      set_real_ip_from 197.234.240.0/22;
+      set_real_ip_from 198.41.128.0/17;
+
+      real_ip_header CF-Connecting-IP;
+    '';
 
     virtualHosts."beautifulblossomgarden.io.vn" = {
       enableACME = true;
@@ -65,8 +88,7 @@
         proxyPass = "http://127.0.0.1:5000";
         proxyWebsockets = true;
         extraConfig = ''
-          limit_req_zone $binary_remote_addr zone=one:10m rate=1r/s;
-          limit_req zone=one burst=5 nodelay;
+          limit_req zone=one burst=10 nodelay;
         '';
        };
     };
