c53ffa556a41c78c0c31372ef3162a8c98ff1adc
theeasternfurry <theeasternfurry@outlook.com>
2026-01-29 11:14:08 +0700

    nixos: update input method kdePackages.fcitx5

--- nixos/input-method.nix
+++ nixos/input-method.nix
@@ -7,7 +7,7 @@
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
-        qt6Packages.fcitx5-unikey
+        kdePackages.fcitx5-unikey
         kdePackages.fcitx5-qt
       ];
     };
