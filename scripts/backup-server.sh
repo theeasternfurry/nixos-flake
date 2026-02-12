b1e853d52c27438b2e006fc0e471b6c8542c7769
theeasternfurry <theeasternfurry@outlook.com>
2026-02-12 07:38:05 +0700

    scripts: update backup-server for veloren and rustfs

--- scripts/backup-server.sh
+++ scripts/backup-server.sh
@@ -12,3 +12,14 @@ rclone copy ~/blossomgarden-trading_*.sql.gz gdrive:Backup-Server/PostgreSQL -P
 
 # Remove backup files
 rm -rf ~/*.sql.gz
+
+# Backup RustFS
+sudo tar -czf ~/rustfs_backup_$(date +%Y%m%d_%H%M%S).tar.gz /var/lib/rustfs
+rclone copy ~/rustfs_*.tar.gz gdrive:Backup-Server/RustFS/ -P
+rm -f ~/rustfs_*.tar.gz
+
+# Backup Veloren
+sudo tar -czf ~/veloren_backup_$(date +%Y%m%d_%H%M%S).tar.gz /var/lib/veloren
+rclone copy ~/veloren_*.tar.gz gdrive:Backup-Server/Veloren/ -P
+rm -f ~/veloren_*.tar.gz
+
