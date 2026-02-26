4283181fb95d5c38cc8c980a4c9f62b708ad34de
theeasternfurry <theeasternfurry@outlook.com>
2026-02-26 12:16:03 +0700

    nixos: disable matrix

--- nixos/services/matrix-continuwuity.nix
+++ nixos/services/matrix-continuwuity.nix
@@ -1,12 +1,12 @@
 {
   services.matrix-continuwuity = {
-    enable = true;
+    enable = false;
     settings = {
       global = {
         server_name = "beautifulblossomgarden.io.vn";
         allow_registration = false;
         allow_encryption = true;
-        allow_federation = true;
+        allow_federation = false;
         max_request_size = 20000000;
         address = [
           "127.0.0.1"
